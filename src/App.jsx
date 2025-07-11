901b15c9172203e546e960c679ad3a9b35af980e
Shivam Singh <shivamsingh.sde@gmail.com>
2025-07-11 20:18:03 +0530

    Add Navigation Bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
-
+import React from "react"
+import About from "./Components/About/About"
+import Contact from "./Components/Contact/Contact"
+import Footer from "./Components/Footer/Footer"
+import Navbar from "./Components/Navbar/Navbar"
+import Skills from "./Components/Skills/Skills"
+import Projects from "./Components/Projects/Projects"
 
 function App() {
  
 
   return (
     <>
-      <h1>
-        Welcome
-        
-      </h1>
+      <div className="bg-[#050512] text-white">
+        <div>
+
+        </div>
+        <div>
+          <Navbar />
+          <About />
+          <Skills />
+          <Projects />
+          <Contact />
+          <Footer />
+        </div>
+      </div>
     </>
   )
 }
